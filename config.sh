a38c2f0e714e39ea12c6a81736032f02d8f0b1d2
bachnxuan <bachnxuan@gmail.com>
2025-07-28 14:44:35 +0700

    chore(config): update config for building esk
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- config.sh
+++ config.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="QuartiX"
+KERNEL_NAME="ESK"
 # Kernel Build variables
-USER="eraselk"
-HOST="gacorprjkt"
-TIMEZONE="Asia/Makassar"
+USER="esk"
+HOST="gki_builder"
+TIMEZONE="Asia/Ho_Chi_Minh"
 # AnyKernel
-ANYKERNEL_REPO="https://github.com/bintang774/anykernel"
-ANYKERNEL_BRANCH="gki"
+ANYKERNEL_REPO="https://github.com/bachnxuan/AnyKernel3"
+ANYKERNEL_BRANCH="android12-5.10"
 # Kernel Source
-KERNEL_REPO="https://github.com/bintang774/gki-android12-5.10"
-KERNEL_BRANCH="quartix/test"
+KERNEL_REPO="https://github.com/bachnxuan/android12-5.10-lts"
+KERNEL_BRANCH="esk/main"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO="https://github.com/bintang774/quartix-releases"
 # Clang
-CLANG_URL="$(./clang.sh mandi-sa)"
+CLANG_URL="$(./clang.sh aosp)"
 CLANG_BRANCH=""
 # Zip name
 # Format: Kernel_name-Linux_version-Variant-Build_date
